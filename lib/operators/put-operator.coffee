bronson/vim-mode-next

fix p cursor position

lib/operators/put-operator.coffee
@@ -58,9 +58,9 @@ class Put extends Operator
       @editor.setCursorScreenPosition(originalPosition)
       @editor.moveToFirstCharacterOfLine()
 
-    @vimState.activateCommandMode()
     if type isnt 'linewise'
       @editor.moveLeft()
+    @vimState.activateCommandMode()
 
   # Private: Helper to determine if the editor is currently on the last row.
   #

t9md/atom-vim-mode-plus

lib/operators/put-operator.coffee
@@ -58,9 +58,9 @@ class Put extends Operator
       @editor.setCursorScreenPosition(originalPosition)
       @editor.moveToFirstCharacterOfLine()
 
-    @vimState.activateNormalMode()
     if type isnt 'linewise'
       @editor.moveLeft()
+    @vimState.activateNormalMode()
 
   # Private: Helper to determine if the editor is currently on the last row.
   #